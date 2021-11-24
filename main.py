9b60366c3144a2dc772f10fc0142c95bb54caf19
Juancho <juan.francisco.torres.valenzuela@uabc.edu.mx>
2021-11-23 21:14:13 -0800

    Juegos randoms con Pipofile

---
+++ main.py
@@ -0,0 +1,10 @@
+from art import *
+from colorama import Fore, Back, Style
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
+print(art("random"))
+print(art("random"))
+print(art("random"))
